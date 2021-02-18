c315cd9e6a99f77351de36b25c91073f7d21c3c0
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2021-02-18 18:34:52 +0300

    comments

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,33 +1,3 @@
-// import { useEffect } from 'react';
-// const escape = 27;
-// function ImagePopup(props) {
-//     useEffect(
-//         () => {
-//             if(!props.isOpen) return;
-//             const handleEscClose =(event) => {
-//                 if((event.keyCode === escape)) {
-//                     props.onClose() 
-//                 }
-//             }
-//             document.addEventListener('keyup', handleEscClose);
-//             return () => {
-//                 document.removeEventListener('keyup', handleEscClose);  
-//             }
-//         }, [props.isOpen, props.onClose]
-//     )
-    
-//     return(
-//         <div className={`popup popup_type_big-photo ${props.isOpen && 'popup_opened'}`} onClick={props.onOvarlayClose}>
-//             <div className="popup__content-photo">
-//                 <button className="popup__close popup__close_type_close-big-foto" type="button" onClick={props.onClose}></button>
-//                 <img  className="popup__photo" src={props.card.link} alt={props.card.name} />
-//             <h3 className="popup__caption">{props.card.name}</h3>
-//             </div>
-//         </div> 
-//     );
-// }
-// export default ImagePopup;
-
 import { useEffect } from 'react';
 const escape = 27;
 function ImagePopup({card, onClose, isOpen, onOvarlayClose}) {
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,35 +1,3 @@
-// import { useEffect } from 'react';
-// const escape = 27;
-// function PopupWithForm(props) {
-//     useEffect(
-//         () => {
-//             if(!props.isOpen) return;
-//             const handleEscClose =(event) => {
-//                 if((event.keyCode === escape)) {
-//                     props.onClose() 
-//                 }
-//             }
-//             document.addEventListener('keyup', handleEscClose);
-//             return () => {
-//                 document.removeEventListener('keyup', handleEscClose);  
-//             }
-//         }, [props.isOpen, props.onClose]
-//     )
-
-//     return (
-//         <div className={`popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`} onClick={props.onOvarlayClose}>
-//                 <div className="popup__content">
-//                 <button className="popup__close" type="button" onClick={props.onClose}></button>
-//                 <h3 className="popup__title">{props.title}</h3>
-//                 <form className={`popup__form popup__form_type_${props.name}`} onSubmit={props.onSubmit} name={props.name} noValidate>
-//                     {props.children}
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// }
-// export default PopupWithForm;
-
 import { useEffect } from 'react';
 const escape = 27;
 function PopupWithForm({name, title , isOpen, onClose, onOvarlayClose, onSubmit, children}) {
